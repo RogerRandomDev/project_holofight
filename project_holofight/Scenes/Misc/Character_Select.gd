d06d47c925a4b177759f8b203c0de39ce3778d0c
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2021-12-21 21:01:19 -0700

    Add files via upload

--- project_holofight/Scenes/Misc/Character_Select.gd
+++ project_holofight/Scenes/Misc/Character_Select.gd
@@ -4,7 +4,9 @@ extends Control
 
 export var character_holder_scene:PackedScene
 export (Array,int)var chars_per_row=[]
-var player_hovering = [0,4]
+#use this for checking current character you are hovering
+#the array is the Y val then the X val
+var player_hovering = [[0,0],[0,4]]
 func _ready():
 	for chars in chars_per_row.size():
 		var char_holder = HBoxContainer.new()
@@ -13,7 +15,8 @@ func _ready():
 		for character in chars_per_row[chars]:
 			var char_held = character_holder_scene.instance()
 			char_holder.add_child(char_held)
-			#change for a check user player_hovering
+			
+			#Change for a check user player_hovering
 			char_held.get_child(1).connect('focus_entered',self,"player_being_hovered",["test"+str(character)])
 		$HSplitContainer/VBoxContainer.add_child(char_holder)
 
